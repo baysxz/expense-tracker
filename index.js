83d526a7dad76be935a1e6fc6931734c0ec306ed
baysxz <baysgalanxz@gmail.com>
2024-10-01 15:06:14 +0800

    a

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { userRouter } from "./router/user.router";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 const port = 8888;
 
 app.use(userRouter);
