4cfcf416803afc73491418bda5bac465c684f384
baysxz <baysgalanxz@gmail.com>
2024-10-04 12:45:10 +0800

    a

--- controller/post/signIn.js
+++ controller/post/signIn.js
@@ -1,14 +1,12 @@
 import { sql } from "../../database/index";
 
 export const signIn = async (request, response) => {
-  const {name, email, password } = request.body;
+  const { name, email, password } = request.body;
   try {
     const users = await sql`SELECT * FROM users
-WHERE userid='1' and email='email'
+WHERE email=${email} AND password=${password}
             `;
-    response
-      .status(200)
-      .json({ user: users});
+    response.status(200).json({ user: users });
   } catch (error) {
     console.log(error);
 
